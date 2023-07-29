fee0199bc2f77a8bd3e32c52734e62889d68e612
Joe Wegner <joe@wegnerdesign.com>
2023-07-29 14:19:44 -0500

    track usernames in chats

--- mentions/followup.js
+++ mentions/followup.js
@@ -2,8 +2,15 @@ const gpt = require('../lib/gpt.js')
 
 module.exports = async (message) => {
   const allMessages = await message.channel.messages.fetch()
+  
+
+  const chatCompletion = await gpt.chat(parseChat(allMessages.reverse()))
+  message.channel.send(chatCompletion.data.choices[0].message)
+}
+
+function parseChat(messages) {
   let chats = []
-  allMessages.reverse().forEach(message => {
+  messages.forEach(message => {
     if(message.author.equals(message.client.user) && !message.system) {
       chats.push({
         role: 'assistant',
@@ -12,11 +19,12 @@ module.exports = async (message) => {
     } else if(message.mentions.has(message.client.user)) {
       chats.push({
         role: 'user',
-        content: message.content.replace(`<@${message.client.user.id}>`, '')
+        content: message.content.replace(`<@${message.client.user.id}>`, ''),
+        name: message.author.username
       })
+      console.log(message.author.username)
     }
   })
 
-  const chatCompletion = await gpt.chat(chats)
-  message.channel.send(chatCompletion.data.choices[0].message)
+  return chats
 }
