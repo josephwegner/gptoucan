d19a49b841a2a33b3d6e57ed6d49dc67d58367c7
Joe Wegner <joe@wegnerdesign.com>
2023-11-26 16:51:51 -0600

    add dall-e support

---
+++ functions/dalle.js
@@ -0,0 +1,27 @@
+const OpenAI = require('openai');
+const openai = new OpenAI({ apiKey: process.env.OPENAI_SECRET_KEY });
+
+module.exports = {
+  definition: {
+    name: 'dalle',
+    description: 'Generate an image using DALL-E',
+    parameters: {
+      type: 'object',
+      properties: {
+        prompt: {
+          type: 'string',
+          description: 'The prompt to pass to DALL-E URL to load'
+        }
+      }
+    }
+  },
+  execute: async function(args) {
+    console.log('executing dalle', args)
+    const image = await openai.images.generate({
+      model: 'dall-e-3',
+      prompt: args.prompt
+    })
+    
+    return image.data[0].url
+  }
+}
